9a7fad377980d7fa9cf720e31ae1e6b1e27cf182
ajayathunt <ajayathunt@gmail.com>
2022-12-06 17:47:17 +0530

    Rout configured for API

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 import express from 'express';
 import { connectToDatabase } from './database';
+import { studentRouter } from './student.routes';
 
 // Load environment variables from the .env file, where the ATLAS_URI is configured 
 dotenv.config();
@@ -16,6 +17,7 @@ if (!ATLAS_URI) {
 connectToDatabase(ATLAS_URI).then(() => {
     const app = express();
     app.use(cors());
+    app.use('/students', studentRouter);
     app.listen(5200, () => {
         console.log('Server running on port http://localhost:5200 ...');
     })
